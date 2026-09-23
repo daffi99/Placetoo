0759149e982567f5bd22573269e4f14061b32086
Daffi Yashir Ghifari <daffidevi@Daffis-MacBook-Air.local>
2026-09-23 10:44:18 +0700

    chore: remove test endpoint

--- api/test.ts
+++
@@ -1,9 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  res.status(200).json({
-    test: true,
-    hasDbUrl: Boolean(process.env.DATABASE_URL),
-    nodeVersion: process.version,
-  });
-}
